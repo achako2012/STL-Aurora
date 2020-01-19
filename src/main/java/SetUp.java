41f8fd21ab9ac8f8df8090f3d783b3e34ed77a7f
a.chako <a.chako@gmail.com>
2020-01-19 17:29:44 +0200

    REST Assured Library SetUped

--- src/main/java/SetUp.java
+++ src/main/java/SetUp.java
@@ -6,6 +6,7 @@ import org.openqa.selenium.support.ui.ExpectedCondition;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
 
+
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
@@ -56,7 +57,7 @@ public class SetUp {
             Thread.sleep(1000);
             getWebDriverWaitInstance().until(expectation);
         } catch (Throwable error) {
-            Assert.fail("Timeout waiting for Page Load Request to complete.");
+            Assert.assertFalse(Boolean.parseBoolean("Timeout waiting for Page Load Request to complete."));
         }
     }
 
