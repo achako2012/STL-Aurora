cc884fc7551714f2d6c0192f15977f6b44f146e0
a.chako <a.chako@gmail.com>
2020-01-22 14:53:13 +0200

    Parse JSON updatre

--- src/test/java/Test.java
+++ src/test/java/Test.java
@@ -1,12 +1,9 @@
-import com.google.gson.Gson;
-import io.restassured.response.Response;
-
 import java.util.ArrayList;
-import java.util.Map;
 
 import static io.restassured.RestAssured.given;
 
 public class Test {
+
 
     @org.testng.annotations.Test
     public void registerToBackOffice() {
@@ -24,22 +21,23 @@ public class Test {
     @org.testng.annotations.Test
     public void findUserByEmail() {
 
-                given()
-                        .param("_dc", "1579519915033")
-                        .param("query", "ochako@markettraders.com")
-                        .param("cust_type", "TRADER")
-                        .param("page", "1")
-                        .param("start", "0")
-                        .param("limit", "25")
-                        .cookies("JSESSIONID", "DDCCE9104E788B6AD94F772FEA68E284")
-                        .when()
-                        .get("https://backoffice-prod01.smarttraderlabs.com/eforex231-admin/admin-app/json/user/find.json")
-                        .then().log().body()
-                        .statusCode(200);
+        ArrayList response = given()
+                .param("_dc", "1579519915033")
+                .param("query", "ochako@markettraders.com")
+                .param("cust_type", "TRADER")
+                .param("page", "1")
+                .param("start", "0")
+                .param("limit", "25")
+                .cookies("JSESSIONID", "ED029E2CB4B8CFB45AC9CF97E039910B")
+                .when()
+                .get("https://backoffice-prod01.smarttraderlabs.com/eforex231-admin/admin-app/json/user/find.json")
+                .then().log().body()
+                .statusCode(200)
+                .extract()
+                .response()
+                .path("data.id");
 
-
-
-
+        System.out.println(response);
     }
 
     @org.testng.annotations.Test
