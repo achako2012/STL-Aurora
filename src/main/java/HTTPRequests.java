82568bea82c5bbb10bac85588a7a54b4ae981658
a.chako <a.chako@gmail.com>
2020-01-23 15:30:23 +0200

    HTTPRequest update

--- src/main/java/HTTPRequests.java
+++ src/main/java/HTTPRequests.java
@@ -1,3 +1,7 @@
+import io.restassured.response.Response;
+
+import java.util.ArrayList;
+
 import static io.restassured.RestAssured.given;
 
 public class HTTPRequests {
@@ -17,44 +21,58 @@ public class HTTPRequests {
         return instance;
     }
 
+    private static String autorizationToken;
+    private static String traderId;
+
     //Methods
     public static void registerToBackOffice() {
 
-        given()
-                .param("l", "MAINTENANCE_ADMIN")
-                .param("p", "fd4e73e45a4a")
+        autorizationToken = given()
+                .contentType("application/x-www-form-urlencoded")
+                .formParam("l", "MAINTENANCE_ADMIN")
+                .formParam("p", "fd4e73e45a4a")
                 .when()
                 .post("https://backoffice-prod01.smarttraderlabs.com/eforex231-admin/admin-app/login")
                 .then().log().cookies()
-                .statusCode(200);
+                .statusCode(302)
+                .extract()
+                .cookie("JSESSIONID");
+
+        System.out.println(autorizationToken);
     }
+
 
     public static void findUserByEmail() {
 
-        given()
+        ArrayList<String> response = given()
                 .param("_dc", "1579519915033")
                 .param("query", "ochako@markettraders.com")
                 .param("cust_type", "TRADER")
                 .param("page", "1")
                 .param("start", "0")
                 .param("limit", "25")
-                .cookies("JSESSIONID", "B0F86477758B60C9F1DAC49788B4DCDF")
+                .cookies("JSESSIONID", autorizationToken)
                 .when()
                 .get("https://backoffice-prod01.smarttraderlabs.com/eforex231-admin/admin-app/json/user/find.json")
                 .then().log().body()
-                .statusCode(200);
+                .statusCode(200)
+                .extract()
+                .response()
+                .path("data.id");
+
+        traderId = response.get(0);
+        System.out.println(traderId);
     }
 
     public static void set360ProTag() {
 
         given()
-
                 .contentType("application/x-www-form-urlencoded")
                 .param("len_type", "N")
                 .param("start_date", "12/17/2019")
-                .param("ref_id", "149579")
+                .param("ref_id", traderId)
                 .param("add", "1075")
-                .cookies("JSESSIONID", "604C0148F6CF7B61F8B40259F7D82B3C")
+                .cookies("JSESSIONID", autorizationToken)
                 .when()
                 .get("https://backoffice-prod01.smarttraderlabs.com/eforex231-admin/admin-app/json/mti/cust_tag_set.json")
                 .then().log().body()
@@ -62,4 +80,3 @@ public class HTTPRequests {
 
     }
 }
-
--- src/test/java/Test.java
+++ src/test/java/Test.java
@@ -1,59 +1,14 @@
-import java.util.ArrayList;
-
-import static io.restassured.RestAssured.given;
-
 public class Test {
 
+    HTTPRequests httpReq = HTTPRequests.getInstance();
 
     @org.testng.annotations.Test
-    public void registerToBackOffice() {
+    public void tagsTest() {
 
-        given()
-                .contentType("application/x-www-form-urlencoded")
-                .formParam("l", "MAINTENANCE_ADMIN")
-                .formParam("p", "fd4e73e45a4a")
-                .when()
-                .post("https://backoffice-prod01.smarttraderlabs.com/eforex231-admin/admin-app/login")
-                .then().log().cookies()
-                .statusCode(200);
-    }
+        httpReq.registerToBackOffice();
+        httpReq.findUserByEmail();
+        httpReq.set360ProTag();
 
-    @org.testng.annotations.Test
-    public void findUserByEmail() {
-
-        ArrayList response = given()
-                .param("_dc", "1579519915033")
-                .param("query", "ochako@markettraders.com")
-                .param("cust_type", "TRADER")
-                .param("page", "1")
-                .param("start", "0")
-                .param("limit", "25")
-                .cookies("JSESSIONID", "ED029E2CB4B8CFB45AC9CF97E039910B")
-                .when()
-                .get("https://backoffice-prod01.smarttraderlabs.com/eforex231-admin/admin-app/json/user/find.json")
-                .then().log().body()
-                .statusCode(200)
-                .extract()
-                .response()
-                .path("data.id");
-
-        System.out.println(response);
-    }
-
-    @org.testng.annotations.Test
-    public void set360ProTag() {
-
-        given()
-                .contentType("application/x-www-form-urlencoded")
-                .param("len_type", "N")
-                .param("start_date", "12/17/2019")
-                .param("ref_id", "108975")
-                .param("add", "1075")
-                .cookies("JSESSIONID", "B0F86477758B60C9F1DAC49788B4DCDF")
-                .when()
-                .get("https://backoffice-prod01.smarttraderlabs.com/eforex231-admin/admin-app/json/mti/cust_tag_set.json")
-                .then().log().body()
-                .statusCode(200);
 
     }
 }
