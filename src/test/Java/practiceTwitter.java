dc5d8d3f213c736192fc0b435ccfc78bce341fd5
Chako2012 <a.chako2012@gmail.com>
2020-02-11 22:53:35 +0200

    refactor

--- src/test/Java/practiceTwitter.java
+++ src/test/Java/practiceTwitter.java
@@ -8,23 +8,31 @@ import static io.restassured.module.jsv.JsonSchemaValidator.matchesJsonSchemaInC
 
 public class practiceTwitter {
 
-    Response response = new TwitterMethods().twitterRequest("AAAAAAAAAAAAAAAAAAAAANRILgAAAAAAnNwIzUejRCOuH5E6I8xnZz4puTs%3D1Zv7ttfk8LF81IUq16cHjhLTvJu4FA33AGWWjCpTnA");
+    Response response;
 
-    @Test
+
+
+
+
+    @Test(description = "Test with valid credentials")
     public void Test1() {
+        response = new TwitterMethods().twitterRequest("AAAAAAAAAAAAAAAAAAAAANRILgAAAAAAnNwIzUejRCOuH5E6I8xnZz4puTs%3D1Zv7ttfk8LF81IUq16cHjhLTvJu4FA33AGWWjCpTnA");
         response.then().statusCode(200);
+        Assert.assertFalse(response.body().asString().isEmpty(), "Empty response body");
+        response.then().assertThat().body(matchesJsonSchemaInClasspath("products-schema.json"));
         System.out.println("Status code - 200");
     }
 
-    @Test
+    @Test(description = "Test with invalid credentials")
     public void Test2() {
-        Assert.assertFalse(response.body().asString().isEmpty(), "Empty response body");
-        System.out.println("Response doesn't empty");
+        response = new TwitterMethods().twitterRequest("test" + "AAAAAAAAAAAAAAAAAAAAANRILgAAAAAAnNwIzUejRCOuH5E6I8xnZz4puTs%3D1Zv7ttfk8LF81IUq16cHjhLTvJu4FA33AGWWjCpTnA");
+        response.then().statusCode(404);
     }
 
-    @Test
-    public void Test4() {
-        response.then().assertThat().body(matchesJsonSchemaInClasspath("products-schema.json"));
+    @Test(description = "Test without token")
+    public void Test3(){
+        response = new TwitterMethods().twitterRequest("");
+        response.then().statusCode(404);
     }
 }
 
